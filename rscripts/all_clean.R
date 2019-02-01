024dad15f0b2a8558fffcfc6fecbc5b4a8801c93
ZenMule <zhangmiao311@gmail.com>
2019-02-01 01:39:38 -0500

    Made the figure.

---
+++ rscripts/all_clean.R
@@ -0,0 +1,24 @@
+## LOAD THE PACKAGE ####
+library(tidyverse)
+
+## READ IN DATA ####
+f1 <- read_csv("data/f1_data.csv")
+f2 <- read_csv("data/f2_data.csv")
+all <- rbind(f1, f2)
+
+# clean the data
+all <- all %>% mutate( # refactor all the categorical variables
+  Footedness = factor(Footedness, levels = c("unfooted", "footed")),
+  Info_str = factor(Info_str, levels = c("background", "broad", "narrow", "contrastive")),
+  Place = factor(Place, levels = c("lower_lip", "tongue_tip", "tongue_body")),
+  Subject_id = factor(Subject_id),
+  Trial_num = factor(Trial_num)
+) %>%
+  select(-Trial_num)
+
+# create separate data sets for Footedness conditions
+all_unf <- filter(all, Footedness == "unfooted")
+all_f <- filter(all, Footedness == "footed")
+
+# save all data
+write_csv(all, "data/all_data.csv")
---
+++ rscripts/all_figures.R
@@ -0,0 +1,66 @@
+## LOAD THE PACKAGE ####
+library(tidyverse)
+library(ggpubr)
+library(RColorBrewer)
+
+## LOAD OBJECTS ####
+all_f_fig <- all_f
+all_unf_fig <- all_unf
+all_fig <- all
+
+## SET COLORS ####
+colors_place <- brewer.pal(3, "Blues")
+color_lp <- colors_place[1]
+color_alv <- colors_place[2]
+color_vl <- colors_place[3]
+
+## FIGURES ####
+# plot gesture duration
+all_gdur_fig <- all_fig %>%
+  ggplot(aes(Info_str, GDUR)) + 
+  geom_boxplot(aes(fill = Place)) +
+  labs(x="Information Structure", 
+       y="Gesture Duration (ms)") +
+  scale_fill_manual(values=c(color_lp, color_alv, color_vl)) +
+  facet_wrap(~ Footedness)
+
+# plot peak velocity
+all_pvel_fig <- all_fig %>%
+  ggplot(aes(Info_str, PVEL)) + 
+  geom_boxplot(aes(fill = Place)) +
+  labs(x="Information Structure", 
+       y="Peak Velocity (mm/s)") +
+  scale_fill_manual(values=c(color_lp, color_alv, color_vl)) +
+  facet_wrap(~ Footedness)
+
+# plot time to peak velocity
+all_t2pvel_fig <- all_fig %>%
+  ggplot(aes(Info_str, T2PVEL)) + 
+  geom_boxplot(aes(fill = Place)) +
+  labs(x="Information Structure", 
+       y="Time-to-Peak Velocity (ms)") +
+  scale_fill_manual(values=c(color_lp, color_alv, color_vl)) +
+  facet_wrap(~ Footedness) 
+
+# plot maximum displacement
+all_mdisp_fig <- all_fig %>%
+  ggplot(aes(Info_str, MDISP)) + 
+  geom_boxplot(aes(fill = Place)) +
+  labs(x="Information Structure", 
+       y="Maximum Displacement (mm)") +
+  scale_fill_manual(values=c(color_lp, color_alv, color_vl)) +
+  facet_wrap(~ Footedness)
+
+# lower lip 4 in 1
+all_plot <- ggarrange(all_gdur_fig, 
+                        all_mdisp_fig, 
+                        all_pvel_fig, 
+                        all_t2pvel_fig, 
+                        ncol = 2, nrow = 2, 
+                        common.legend = T, legend="bottom")
+all_plot <- annotate_figure(all_plot,
+                              top = text_grob("F2 Lower Lip Data", color = "black", face = "bold", size = 14))
+
+pdf("figures/all_plot.pdf")
+all_plot
+dev.off()
